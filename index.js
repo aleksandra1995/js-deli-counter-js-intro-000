1c91aa8495ec9897fd84f56eb2eb3d141ae4691c
Aleksandra Kosc <aleksandra.kosc@gmail.com>
2019-06-06 23:04:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-function takeANumber(line, name) {
-   line.push(name)
-  return (`Welcome, ${name}. You are number ${line.length} in line.`); 
-  }
-  function nowServing(line) {
-  if (line.length === 0) { 
-    return "There is nobody waiting to be served!" }
-    return `Currently serving ${line.shift()}.`
-}
-// why couldnt i use else? i had to use return. 
-function currentLine(line) {
-  if (line.length === 0) { 
-    return "The line is currently empty."}
-    var number = []
-    for (var i = 0; i < line.length; i++) {
-      number.push(`${i + 1}. ${line[i]}`) }
-     return `The line is currently: ${number.join(', ')}`
-     }
+
      //interpolatig cause its easier then string concatinating 
      //string interpolation 
