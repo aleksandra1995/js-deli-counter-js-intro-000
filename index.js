0235e875291f29c07dfa572c581a414e913d3113
Aleksandra Kosc <aleksandra.kosc@gmail.com>
2019-06-06 22:15:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function takeANumber(line, name) {
   function nowServing(line) {
     if (line.length === 0)
     {return "There is nobody waiting to be served!"} 
-    else {`Currently serving ${line.shift}`}
+    return `Currently serving ${line.shift}`
     
   }
