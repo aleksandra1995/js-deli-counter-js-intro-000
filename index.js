e94b43de1dceeec8d2c88732d1c7845a6e6efe8f
Aleksandra Kosc <aleksandra.kosc@gmail.com>
2019-06-06 23:39:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,4 +11,7 @@ function nowServing(line) {
   function currentLine(line) {
     var i = 0
     return `The line is currently ${i + 1}${line.length[i]}`
+    if (line.length === 0) {
+      return "The line is currently empty."
+    }
   }
