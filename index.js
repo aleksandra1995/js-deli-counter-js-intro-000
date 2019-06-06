41f347608de1c90d0ff2ea7321a448f94c37004d
Aleksandra Kosc <aleksandra.kosc@gmail.com>
2019-06-06 22:11:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,4 @@ function takeANumber(line, name) {
   //it wasnt an arry so the index number wasnt 0. when tried
   // to put in array i was getting 0 insted of 1 
   function nowServing(line) {
-    if (0> line.length) {
-      line.shift()
-      return line
-  } else {"There is nobody waiting to be served"} }
+    if ( {"There is nobody waiting to be served"} }
