63b1194f903131541c113ea66eacd5664982242a
Aleksandra Kosc <aleksandra.kosc@gmail.com>
2019-06-06 22:03:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber(line, name) {
-  var message = []
-  message.push(`Welcome, ${name}. You are number ${line.length} in line.`); 
+   line.push(name)
+  (`Welcome, ${name}. You are number ${line.length} in line.`); 
   return message }
