8f985c8f7365a9aa5b8fb9668747df770d7bc752
Aleksandra Kosc <aleksandra.kosc@gmail.com>
2019-06-07 00:49:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,18 +8,7 @@ function nowServing(line) {
   } else { return `Currently serving ${line.shift()}.` } }
   // else was not working because i wasnt using return with it. 
   
-  function currentLine(line) {
-    if (line.length === 0) {
-      return "The line is currently empty."}
-      var newLine = []
-      for (var i = 0; i < line.length; i++) { 
-        if (line.length - 1)
-     { newLine.push(`The line is currently: ${i + 1}. ${line[i]}`)
-       return newLine
-     } 
-      else { newLine.push(`The line is currently: ${i + 1}. ${line[i]},`)
-        return newLine } }}  
-        
+  
         
         /*did not work. .toSlice did not work either. */
       
