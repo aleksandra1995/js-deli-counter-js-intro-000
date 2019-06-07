afba11c30250bd8475ea667c50eaafbb659510c4
Aleksandra Kosc <aleksandra.kosc@gmail.com>
2019-06-07 01:00:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ function takeANumber(line, name) {
   line.push(name)
   return `Welcome, ${name}. You are number ${line.length} in line.`
 }
-// 
+// used interpolation insted of concatenation becuase it looks cleaner. tried using a for loop but the number kept starting at 0
+
 function nowServing(line) {
   if (line.length === 0) {
     return "There is nobody waiting to be served!"
   } else { return `Currently serving ${line.shift()}.` } }
+  
   // else was not working because i wasnt using return with it. 
   
   function currentLine(line) {
@@ -17,7 +19,7 @@ function nowServing(line) {
         newLine.push(`${i + 1}. ${line[i]}`) }
    return `The line is currently: ${newLine.join(', ')}`
       } 
-        
+        /* tried .toString, tried if, else if, and else - didnt work. 
       
     
     
