d1aedfc460d41e656b48bcc6579af1b711497682
Aleksandra Kosc <aleksandra.kosc@gmail.com>
2019-06-07 00:53:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function takeANumber(line, name) {
   line.push(name)
   return `Welcome, ${name}. You are number ${line.length} in line.`
 }
+// 
 function nowServing(line) {
   if (line.length === 0) {
     return "There is nobody waiting to be served!"
