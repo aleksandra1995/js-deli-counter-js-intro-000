185ad5fe559c3350d345df4f10009696752ee33d
Aleksandra Kosc <aleksandra.kosc@gmail.com>
2019-06-07 00:25:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,8 +13,9 @@ function nowServing(line) {
       return "The line is currently empty."}
       var lineNew = [] 
       for (var i = 0; i < line.length; i++);
-    { else if (line.length-1)
-      return lineNew.push(`The line is currently: ${i + 1} . ${line.length[i]}`)} }
+    {if (line.length-1)
+      return lineNew.push(`The line is currently: ${i + 1} . ${line.length[i]}`)} 
+      else { return lineNew.push(`The line is currently: ${i + 1} . ${line.length[i]},`)} }
     
     
   
